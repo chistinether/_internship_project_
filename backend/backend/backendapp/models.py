e5e616b6b358e4fab282dea6e35a93a095c2ef55
angellamurungi91-snow <angellamurungi91@gmail.com>
2026-06-14 10:37:32 -0700

    made changes

--- backend/backend/backendapp/models.py
+++ backend/backend/backendapp/models.py
@@ -13,6 +13,7 @@ class User(AbstractUser):
     #is_verified = models.BooleanField(default=False)
 
     def __str__(self):
+        return f"{self.username} ({self.role})"
     
 
 class Student(models.Model):
